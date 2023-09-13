7a975ac6b0dc4d8cd1d49605490bc312c672f2cf
Feodor <kologermit@gmail.com>
2023-09-13 10:49:51 +0400

    Перенёс учёбу в отдельную репу

--- api_php/public/index.php
+++ api_php/public/index.php
@@ -3,13 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>UDSU KOLOGERMIT PHP LEARNING</title>
+	<title>FEBANG API DOC</title>
 </head>
 <body>
-	Доступные функции:
-	<p><a href="square-roots.php">Вычисление корней</a></p>
-	<p><a href="triangle-angles.php">Вычисление углов треутольника</a></p>
-	<p><a href="triangle-area.php">Вычисление площади треутольника</a></p>
-	<p><a href="triangle-sides.php">Вычисление сторон треутольника</a></p>
 </body>
 </html>
--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import $ from 'jquery';
+
+$.ajax({"url": "api/udsu/triangle-angles.php", "success": (request) => {console.log(request);}});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
